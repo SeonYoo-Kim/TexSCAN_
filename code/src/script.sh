ae7a8abd625b425c1b0cb8bfece0ff98c8e61c49
theta <tjdn13@naver.com>
2024-08-19 15:50:06 +0900

    all

--- code/src/script.sh
+++ code/src/script.sh
@@ -1,12 +1,24 @@
-python main.py --e 50 --m 600
-python main.py --e 50 --m 550
-python main.py --e 50 --m 500
-python main.py --e 50 --m 450
-python main.py --e 50 --m 400
-python main.py --e 50 --m 350
-python main.py --e 50 --m 300
-python main.py --e 50 --m 250
-python main.py --e 50 --m 200
-python main.py --e 50 --m 150
-python main.py --e 50 --m 100
-python main.py --e 50 --m 50
+python main.py --e 50 --m 1000
+python main.py --e 50 --m 950
+python main.py --e 50 --m 900
+python main.py --e 50 --m 850
+python main.py --e 50 --m 800
+python main.py --e 50 --m 750
+python main.py --e 50 --m 700
+python main.py --e 50 --m 650
+
+
+
+
+#python main.py --e 50 --m 600
+#python main.py --e 50 --m 550
+#python main.py --e 50 --m 500
+#python main.py --e 50 --m 450
+#python main.py --e 50 --m 400
+#python main.py --e 50 --m 350
+#python main.py --e 50 --m 300
+#python main.py --e 50 --m 250
+#python main.py --e 50 --m 200
+#python main.py --e 50 --m 150
+#python main.py --e 50 --m 100
+#python main.py --e 50 --m 50
